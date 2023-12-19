54d5e32504d0d8f91755d6d7edc92f1a7f1d5d59
Xinxuan Zhuo <1575165952@qq.com>
2023-12-19 11:14:50 +0800

    add: 23.12.19

--- heima-spring5-senior/src/main/java/org/zhuo/zother/qrcode/domain/QRCodeService.java
+++ heima-spring5-senior/src/main/java/org/zhuo/zother/qrcode/domain/QRCodeService.java
@@ -18,7 +18,7 @@ import org.zhuo.zother.qrcode.handler.TemplateHandler;
 
 import javax.annotation.Resource;
 import java.io.ByteArrayOutputStream;
-
+import java.nio.charset.StandardCharsets;
 
 
 import static org.zhuo.zother.qrcode.entity.QRCodeEntity.HEIGHT;
@@ -60,20 +60,21 @@ public class QRCodeService {
         try{
             // 生成二维码图片
             QRCodeWriter writer = new QRCodeWriter();
-            String context = MakeHtmlDomain+htmlStr;
-            BitMatrix matrix = writer.encode(context, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
-
+            String context = MakeHtmlDomain + htmlStr;
+            // 将文本内容转换为UTF-8编码的字节数组
+            byte[] contextBytes = context.getBytes(StandardCharsets.UTF_8);
+            // 使用ISO-8859-1编码解析字节数组
+            BitMatrix matrix = writer.encode(new String(contextBytes, StandardCharsets.ISO_8859_1), BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
             // 将二维码图片写入字节数组输出流中
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             MatrixToImageWriter.writeToStream(matrix, "PNG", outputStream);
-
             // 创建字节数组资源对象
             byte[] bytes = outputStream.toByteArray();
             ByteArrayResource resource = new ByteArrayResource(bytes);
-
             // 构建响应头，指定Content-Disposition为attachment，文件名为qrcode.png
             HttpHeaders headers = new HttpHeaders();
             headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=qrcode.png");
+            headers.set(HttpHeaders.CONTENT_TYPE, "image/png; charset=UTF-8");
             return ResponseEntity.ok()
                     .headers(headers)
                     .contentLength(bytes.length)
