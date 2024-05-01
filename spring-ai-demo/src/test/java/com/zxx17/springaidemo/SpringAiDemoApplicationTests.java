6e4a5d952f676d8bd990b7bca4809558dc4f6eeb
Xinxuan Zhuo <1575165952@qq.com>
2024-05-01 20:29:57 +0800

    add：040501 spring-ai-model

---
+++ spring-ai-demo/src/test/java/com/zxx17/springaidemo/SpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxx17.springaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
