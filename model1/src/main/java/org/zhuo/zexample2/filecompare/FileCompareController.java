c3d1c06c0ca1d483e7133c1d3e284b312d88d56d
Xinxuan Zhuo <1575165952@qq.com>
2024-05-06 11:37:14 +0800

    add：2405006 file_compare(use aspose)

---
+++ model1/src/main/java/org/zhuo/zexample2/filecompare/FileCompareController.java
@@ -0,0 +1,38 @@
+package org.zhuo.zexample2.filecompare;
+
+import com.aspose.words.Document;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+/**
+ * @author Xinxuan Zhuo
+ * @version 2024/5/6
+ * <p>
+ *
+ * </p>
+ */
+
+@RestController
+@RequestMapping("/file-compare")
+public class FileCompareController {
+
+    @PostMapping
+    public void compare(@RequestPart("file1")MultipartFile file1, @RequestPart(name = "file2") MultipartFile file2) throws Exception {
+        // TODO
+    }
+
+    public static void main(String[] args) throws Exception {
+        Document docA = new Document("E:\\文件1.docx");
+        Document docB = new Document("E:\\文件2.docx");
+
+
+        // There should be no revisions before comparison.
+        docA.acceptAllRevisions();
+        docB.acceptAllRevisions();
+
+        docA.compare(docB, "Author Name", new Date());
+        docA.save("E:\\Output.docx");
+    }
+}
