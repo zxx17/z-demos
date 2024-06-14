e4ec334cec573cc729f9055008e9a35e00ef1565
Xinxuan Zhuo <1575165952@qq.com>
2024-06-14 12:51:50 +0800

    feat(model2): initialize new Spring Boot application
    - Create new Spring Boot project with version 3.0.1.
    - Add necessary dependencies for web and test functionalities.
    - Configure Maven build plugins and exclude Lombok from the build.- Set Java version to 17.
    - Add basic application structure including main class and tests.

---
+++ model2/src/main/java/org/zxx17/model2/Model2Application.java
@@ -0,0 +1,13 @@
+package org.zxx17.model2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Model2Application.class, args);
+  }
+
+}
---
+++ model2/src/test/java/org/zxx17/model2/Model2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zxx17.model2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Model2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
